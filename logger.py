80b7d827f42c3c0ea3bdc25e655732f78e308058
thejeshgn <i@thejeshgn.com>
2014-12-31 15:48:21 +0530

    First run on ACT

--- logger.py
+++ logger.py
@@ -7,57 +7,74 @@ import dataset
 import time
 from datetime import datetime
 import json
-t = datetime.now()
-start_time= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-log = {}
-log["TIME_START"]= str(start_time),
 
-
+RUN_NAME = "FIRST_RUN"
     
-#implement this
+#TODO: implement this using API
 def getISPDetails():
-    log["ISP"]= "Atria Convergence Technologies Pvt. Ltd.",
-    log["ISP"]=  "103.227.96.80",
-    log["IP_LOCATION"]= "India, Karnataka, Bangalore",
+    log = {}
+    log["ISP"]= "Atria Convergence Technologies Pvt. Ltd."
+    log["IP"]=  "103.227.96.80"
+    log["ISP_SHORT"]=  "ACT"    
+    log["IP_LOCATION"]= "India, Karnataka, Bangalore"
     return log
 
 
-#get the rules for error_msgs
+#TODO:get the rules for error_msgs
 def checkForISP(isp, content):
     soup = BeautifulSoup(content)
     title = soup.find('title')
-    if title == "URL Blocked!":
-        return "BLOCKED"
+    if title and len(title.contents) > 0:
+        if str(title.contents[0]) == "URL Blocked!":
+            return "BLOCKED"
+        else:
+            return "CANT DECIDE"
     else:
         return "CANT DECIDE"
 
-URLS = []
-log.update(getISPDetails())
-
+db = dataset.connect('sqlite:///./database/db.sqlite')
+URL_TABLE = db['URLS']
 with open("urls.txt") as f:
     for line in f:
         #each line is a link
         url_dict = {}
         url = str(line).strip()
-        r = requests.get(url)
-        url_dict['URL']=url
-        if r.status_code == 200:
-            content = r.content
-            isp = "Atria Convergence Technologies Pvt. Ltd."
-            status = checkForISP(isp, content)
-            url_dict["STATUS"]=status
-        else:
-            url_dict["STATUS"]= "UNREADCHABLE, HTTP STATUS ="+str(r.status_code)
-        print url_dict    
-        URLS.append(url_dict) 
-log['URLS']=URLS
-            
-            
-            
-end_time= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-log["TIME_END"] = end_time
-end_time_log_file_name= "status_log_"+str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))+".json"
+        print "Trying ="+(url)
+        results = URL_TABLE.find_one(URL=url, RUN_NAME=RUN_NAME)
+        if results:
+            print "Exists"
+            continue
+        r = None
+        try:
+            r = requests.get(url)        
+        except requests.exceptions.Timeout:
+            # Maybe set up for a retry, or continue in a retry loop
+            url_dict["STATUS"]= "UNREACHABLE, Timeout"
+        except requests.exceptions.TooManyRedirects:
+            # Tell the user their URL was bad and try a different one
+            url_dict["STATUS"]= "UNREACHABLE, TooManyRedirects"
 
-f = open('myfile','w')
-f.write(json.dumps(log))
-f.close() 
+        except requests.exceptions.RequestException as e:
+            # catastrophic error. bail.
+            url_dict["STATUS"]= "UNREACHABLE, RequestException"
+
+    
+        url_dict['URL']=url
+        isp = "Atria Convergence Technologies Pvt. Ltd."
+        if r:
+            if r.status_code == 200:
+                content = r.content
+                status = checkForISP(isp, content)
+                url_dict["STATUS"]=status            
+            else:
+                url_dict["STATUS"]= "UNREACHABLE, HTTP STATUS ="+str(r.status_code)
+
+        url_dict.update(getISPDetails())
+        start_time= datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        url_dict["RUN_NAME"]=RUN_NAME
+        url_dict["RUN_TIME"]=start_time
+        print url_dict  
+        URL_TABLE.insert(url_dict)
+        db.commit()
+        time.sleep(1)
+
