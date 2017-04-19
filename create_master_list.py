63524e988092337a1f2b6bec29f7f357a50f1d3c
Thejesh GN <i@thejeshgn.com>
2017-04-19 10:27:01 +0530

    updated master list

--- create_master_list.py
+++ create_master_list.py
@@ -12,13 +12,17 @@ db = dataset.connect('sqlite:///./database/db.sqlite')
 MASTER_LIST= db['MASTER_LIST']
 
 def loadLists():    
-    for files in ['blocked-websites-MSM-world-cup.txt', 'kafila_maste_list.txt', 'medianama2.txt', 'toi_govt_block_32_websites.txt','india_music_industry_104_sites.txt']:
+    for files in ['blocked-websites-MSM-world-cup.txt', 'kafila_maste_list.txt', 'medianama2.txt', 'toi_govt_block_32_websites.txt','india_music_industry_104_sites.txt', 'madras_court_order_kabali_forum_sites.txt', 'madras_court_order_kabali_streaming_sites.txt', 'madras_court_order_kabali_torrent_sites.txt']:
         file_path = "./lists/"+files
         with open(file_path) as f:
             for line in f:
                 url_dict = {}
                 URL = str(line).strip()
                 print URL
+                if URL.startswith('https://') or URL.startswith('http://'):
+                    continue
+                else:
+                    URL = "http://"+URL
                 SOURCE =  str(files)
                 results = MASTER_LIST.find_one(URL=URL)
                 if results:
